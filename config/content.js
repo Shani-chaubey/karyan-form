ddb4d65b182223f848fd9373b372ca959ca3b5ab
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2026-05-07 14:08:51 +0530

    done

--- config/content.js
+++ config/content.js
@@ -3,7 +3,7 @@ export const eventDate = "18 May 2026";
 
 // Form closes at midnight on this date (ISO format)
 // Set this to the registration deadline
-export const formCutoffDate = new Date("2026-05-10T23:59:59");
+export const formCutoffDate = new Date("2026-05-09T11:00:00");
 
 export const heroTagline = "Join Us for the Grand Launch of the Project";
 export const successMessage =
